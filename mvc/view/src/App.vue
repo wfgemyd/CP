2a8f23df32fda13cd5e85d2bd10676e63c09e56e
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2024-03-18 15:51:44 +0100

    Structure and first page

--- mvc/view/src/App.vue
+++ mvc/view/src/App.vue
@@ -1,31 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// Import the login component with a relative path
+import LoginComponent from './components/login.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LoginComponent
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <!-- Main application container -->
+    <header>
+      <!-- Your app header -->
+    </header>
+    <main>
+      <!-- Main content area -->
+      <!-- Dynamically include the login component here -->
+      <LoginComponent />
+    </main>
+    <footer>
+      <!-- Your app footer -->
+    </footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style >
 
+
+<style lang="sass">
+// Importing global styles
+@import './styles/_index.sass'
+#app
+  background: $background-color
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100vh
+
+  .login-page
+    display: flex
+    justify-content: center
+    align-items: center
+    height: 100%
+</style>
---
+++ mvc/view/src/components/login.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+  <div class="first_page_login_container login-page">
+    <img id="company_logo" src="../assets/nxp__logo.svg.png" alt="company log" title="NXP">
+    <div class="welcoming_msg"><h1>Member Log In</h1></div>
+    <div class="wbi_login_container">
+      <form action="">
+        <div class="input_container">
+          <label class="input_label" for="email">Email:</label>
+          <input type="text" id="username" name="username" placeholder="Enter your WBI" required>
+        </div>
+        <div class="input_container">
+          <label class="input_label" for="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required>
+        </div>
+        <div class="login_btn">
+          <button type="submit">Log In</button>
+        </div>
+      </form>
+
+  </div>
+  </div>
+
+
+</template>
+
+<style lang="sass">
+@import '../styles/main.sass'
+</style>
