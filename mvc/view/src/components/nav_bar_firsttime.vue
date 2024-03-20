0f6e36472f73d835c2cc1328dc4c1937b1acb064
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2024-03-20 20:16:51 +0100

    updates

--- mvc/view/src/components/nav_bar_firsttime.vue
+++ mvc/view/src/components/nav_bar_firsttime.vue
@@ -3,20 +3,7 @@
 </script>
 
 <template>
-  <div class="navbar-container">
-    <nav class="onboarding_nav">
-      <div id="nav-left">
-        <a href="#" title="Home page"> <img class="nav-logo" src="../assets/nxp__logo.svg.png" alt="firm logo"> </a>
-      </div>
-      <ul id="nav_items">
-        <li class="user-info"> <!-- Changed to class for reuse -->
-          <div>Full name</div>
-          <div class="wbi">WBI</div>
-        </li>
-        <li><a class="logout-btn">></a></li>
-      </ul>
-    </nav>
-  </div>
+
 </template>
 
 
--- mvc/view/src/components/onboarding.vue
+++ mvc/view/src/components/onboarding.vue
@@ -1,9 +1,74 @@
 <script setup>
-import nav_bar_firsttime from './nav_bar_firsttime.vue';
+import { ref } from 'vue';
+
+const checked = ref(false);
+const itemText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+    ' Accusamus cumque dolor doloribus esse ipsa, ipsam magni molestias temporibus ' +
+    'voluptatem. Doloremque facilis inventore libero vel? Accusamus explicabo id nihil ' +
+    'quaerat quod?\n';
+
+
 </script>
 
 <template>
-  <nav_bar_firsttime />
+  <div class= "wrapper2">
+    <div class="onboarding_container">
+
+      <div class="navbar-container">
+        <nav class="onboarding_nav">
+          <div id="nav-left">
+            <a href="#" title="Home page"> <img class="nav-logo" src="../assets/nxp__logo.svg.png" alt="firm logo"> </a>
+          </div>
+          <ul id="nav_items">
+            <li class="user_picture"> <img src = "../assets/derp.png" alt="profile_pic"> </li>
+            <li class="user-info"> <!-- Changed to class for reuse -->
+              <div>Full name</div>
+              <div class="wbi">@WBI</div>
+            </li>
+            <li><a href="../" title="Logout" class="logout-btn"><img src = "../assets/logout.png" alt="logoutbtn"> </a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="onboarding_window">
+
+        <div class="onboarding_top">
+          <h1 class="header">Onboarding Checklist</h1>
+          <img class="colorfil_underline" src= "../assets/colorfull_underline.png" alt="underline">
+        </div>
+
+        <div class="checklist">
+          <div class="item">
+            <div class="checkbox"><img class= "checkbox_vec" src="../assets/vector.png" alt="checkbox" ></div>
+            <div class="text">{{ itemText }}</div>
+          </div>
+          <div class="item">
+            <div class="checkbox"><img class= "checkbox_vec" src="../assets/vector.png" alt="checkbox" ></div>
+            <div class="text">{{ itemText }}</div>
+          </div>
+          <div class="item">
+            <div class="checkbox"><img class= "checkbox_vec" src="../assets/vector.png" alt="checkbox" ></div>
+            <div class="text">{{ itemText }}</div>
+          </div>
+          <div class="item">
+            <div class="checkbox"><img class= "checkbox_vec" src="../assets/vector.png" alt="checkbox" ></div>
+            <div class="text">{{ itemText }}</div>
+          </div>
+          <div class="item">
+            <div class="checkbox"><img class= "checkbox_vec" src="../assets/vector.png" alt="checkbox" ></div>
+            <div class="text">{{ itemText }}</div>
+          </div>
+        </div>
+
+        <div class="finalize_checklist">
+          <button class="finalize_btn">Submit</button>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
 </template>
 
 <style scoped lang="sass">
